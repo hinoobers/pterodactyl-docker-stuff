96a5a73f1f5140067208b2a4dbf2347b6172607d
hinoobers <87904488+hinoobers@users.noreply.github.com>
2023-02-17 00:29:23 +0200

    Update entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
