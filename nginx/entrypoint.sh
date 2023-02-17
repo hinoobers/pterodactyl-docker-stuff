aad8fa6ee2e0316c029fd10c1afb9a90c68fc942
hinoobers <87904488+hinoobers@users.noreply.github.com>
2023-02-17 21:50:10 +0200

    Update entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-sleep 1
-
 cd /home/container
 
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print Git Version
+git --version
+
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
